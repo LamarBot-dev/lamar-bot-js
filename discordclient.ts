a2fe3283db8dbcd14e2e35c3dd3bfa3710dbbd9e
Ugric <epicugric@gmail.com>
2023-05-12 18:47:36 +0100

    fix not working in dms

--- discordclient.ts
+++ discordclient.ts
@@ -1,14 +1,24 @@
-import Discord, { GatewayIntentBits } from "discord.js";
+import Discord, { GatewayIntentBits, Partials } from "discord.js";
 const client = new Discord.Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildIntegrations,
         GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMessageTyping,
+    ],
+    partials: [
+        Partials.Channel, // Required to receive DMs
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.User,
+        Partials.GuildScheduledEvent
     ],
 });
 export { Discord, client };
