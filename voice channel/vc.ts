dbe0ddb70e410780e3fb6ee508a0eddf5597d098
Ugric <epicugric@gmail.com>
2023-05-30 13:34:03 +0100

    make embeds

--- voice channel/vc.ts
+++ voice channel/vc.ts
@@ -14,6 +14,8 @@ import {
     joinVoiceChannel,
 } from "@discordjs/voice";
 import path from "path";
+import errorMessage from "../error_message";
+import { Discord } from "../discordclient";
 
 const player = createAudioPlayer({
     behaviors: {
@@ -67,12 +69,24 @@ const joinVC: commandFunctionType = async (interaction) => {
     switch (connection) {
         case 1:
             await interaction.editReply({
-                content: "You must be in a voice channel to use this command.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "You must be in a voice channel to use this command.",
+                        null
+                    ),
+                ],
             });
             break;
         case 2:
             await interaction.editReply({
-                content: "This command can only be used in a server.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "This command can only be used in a server.",
+                        null
+                    ),
+                ],
             });
             break;
         default:
@@ -90,19 +104,44 @@ const playRadio: commandFunctionType = async (interaction) => {
     switch (connection) {
         case 1:
             await interaction.editReply({
-                content: "You must be in a voice channel to use this command.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "You must be in a voice channel to use this command.",
+                        null
+                    ),
+                ],
             });
             return;
         case 2:
             await interaction.editReply({
-                content: "This command can only be used in a server.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "This command can only be used in a server.",
+                        null
+                    ),
+                ],
             });
             return;
     }
 
     connection[0].subscribe(player);
     await interaction.editReply({
-        content: `Playing \`Radio Los Santos\` in ${connection[1]}`,
+        embeds: [
+            new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: interaction.user.tag,
+                    iconURL: interaction.user.avatarURL() || undefined,
+                })
+                .setTitle("Playing Radio Los Santos")
+                .setThumbnail(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/infomation%20icon.png?raw=true"
+                )
+                .setImage(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/dancing%20lamar.gif?raw=true"
+                ),
+        ],
     });
 };
 
@@ -114,47 +153,99 @@ const stopvc: commandFunctionType = async (interaction) => {
         !interaction.guild
     ) {
         await interaction.editReply({
-            content: "This command can only be used in a server.",
+            embeds: [
+                errorMessage(
+                    interaction,
+                    "This command can only be used in a server.",
+                    null
+                ),
+            ],
         });
         return;
     }
     const connection = getVoiceConnection(interaction.guildId);
     if (!connection) {
         await interaction.editReply({
-            content: "I am not in a voice channel.",
+            embeds: [
+                errorMessage(
+                    interaction,
+                    "LamarBot not in a voice channel.",
+                    null
+                ),
+            ],
         });
         return;
     }
     const dispatch = connection.subscribe(createAudioPlayer());
     dispatch?.unsubscribe();
     await interaction.editReply({
-        content: "Stopped playing audio.",
+        embeds: [
+            new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: interaction.user.tag,
+                    iconURL: interaction.user.avatarURL() || undefined,
+                })
+                .setTitle("Stopped Radio Los Santos")
+                .setThumbnail(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/infomation%20icon.png?raw=true"
+                )
+                .setImage(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/confused%20lamar.png?raw=true"
+                ),
+        ],
     });
 };
 
 const roastvc: commandFunctionType = async (interaction) => {
-     await interaction.deferReply();
+    await interaction.deferReply();
     const connection = await runJoinVC(interaction);
     switch (connection) {
         case 1:
             await interaction.editReply({
-                content: "You must be in a voice channel to use this command.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "You must be in a voice channel to use this command.",
+                        null
+                    ),
+                ],
             });
             return;
         case 2:
             await interaction.editReply({
-                content: "This command can only be used in a server.",
+                embeds: [
+                    errorMessage(
+                        interaction,
+                        "This command can only be used in a server.",
+                        null
+                    ),
+                ],
             });
             return;
     }
-    const roastreasource = createAudioResource(path.join(__dirname, "./roast/roast.mp3"));
+    const roastreasource = createAudioResource(
+        path.join(__dirname, "./roast/roast.mp3")
+    );
     const roastplayer = createAudioPlayer();
     roastplayer.play(roastreasource);
     connection[0].subscribe(roastplayer);
     await interaction.editReply({
-        content: `Roasting everyone in ${connection[1]}`,
+        embeds: [
+            new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: interaction.user.tag,
+                    iconURL: interaction.user.avatarURL() || undefined,
+                })
+                .setTitle(`Roasting everyone in ${connection[1]}`)
+                .setThumbnail(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/infomation%20icon.png?raw=true"
+                )
+                .setImage(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/voice%20channel/roast/roast.gif?raw=true"
+                ),
+        ],
     });
-}
+};
 
 const disconnectvc: commandFunctionType = async (interaction) => {
     await interaction.deferReply();
@@ -164,22 +255,46 @@ const disconnectvc: commandFunctionType = async (interaction) => {
         !interaction.guild
     ) {
         await interaction.editReply({
-            content: "This command can only be used in a server.",
+            embeds: [
+                errorMessage(
+                    interaction,
+                    "This command can only be used in a server.",
+                    null
+                ),
+            ],
         });
         return;
     }
     const connection = getVoiceConnection(interaction.guildId);
     if (!connection) {
         await interaction.editReply({
-            content: "I am not in a voice channel.",
+            embeds: [
+                errorMessage(
+                    interaction,
+                    "LamarBot not in a voice channel.",
+                    null
+                ),
+            ],
         });
         return;
     }
     connection.destroy();
     await interaction.editReply({
-        content: "Disconnected from voice channel.",
+        embeds: [
+            new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: interaction.user.tag,
+                    iconURL: interaction.user.avatarURL() || undefined,
+                })
+                .setTitle("Disconnected from voice channel")
+                .setThumbnail(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/infomation%20icon.png?raw=true"
+                )
+                .setImage(
+                    "https://github.com/Ugric/lamar-bot-js/blob/main/images/confused%20lamar.png?raw=true"
+                ),
+        ],
     });
 };
-            
 
 export { joinVC, playRadio, stopvc, disconnectvc, roastvc };
